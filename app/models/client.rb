08bb242b7d8a6137ec7ae54e6121580236f87095
laura <laura_lopez_bukovac@hotmail.com>
2013-05-23 09:41:12 -0300

    Delete user when company or client is deleted

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ActiveRecord::Base
-  has_one :user
+  has_one :user, :dependent => :destroy
   has_many :titles, :through => :client_title
 
   attr_accessible :address_id, :first_name, :last_name, :user_id
