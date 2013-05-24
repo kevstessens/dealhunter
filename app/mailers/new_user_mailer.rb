9332fc7d22fe34d149fcf0e348c5dacb6d7cb39f
kevstessens <kevstessens@gmail.com>
2013-05-24 13:51:42 -0300

    New Company user mailer

---
+++ app/mailers/new_user_mailer.rb
@@ -0,0 +1,9 @@
+class NewUserMailer < ActionMailer::Base
+  default :from => "Dealhunter Argentina"
+
+  def new_user_email(user)
+    @user = user
+    mail(:to => user.email, :subject => "Bienvenido a Dealhunter!")
+  end
+
+end
