c563071077e2d23c3f3ed08dbde08d1f4433cd85
laura <laura_lopez_bukovac@hotmail.com>
2013-05-29 08:48:25 -0300

    Define address relationships

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ActiveRecord::Base
   belongs_to :client
   belongs_to :branch
+  has_one :country
 
   attr_accessible :city, :country_id, :floor, :number, :state, :street
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,20 @@
 class Client < ActiveRecord::Base
   belongs_to :user, :dependent => :destroy
   has_many :titles, :through => :client_title
+  has_one :address
 
-  attr_accessible :address_id, :first_name, :last_name, :user_id
+  attr_accessible :address_id, :first_name, :last_name, :user_id, :address_attributes
 
   validates_presence_of :user_id
+
+
+  accepts_nested_attributes_for :address, :allow_destroy => true,
+                                :reject_if => proc { |attributes|
+                                  attributes['city'].blank? and
+                                      attributes['country'].blank? and
+                                      attributes['number'].blank? and
+                                      attributes['postcode'].blank? and
+                                      attributes['state'].blank? and
+                                      attributes['street'].blank?
+                                }
 end
