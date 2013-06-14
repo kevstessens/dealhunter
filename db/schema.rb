e5e95e4f86605107450b6c6b51e2fab44beec92f
laura <laura_lopez_bukovac@hotmail.com>
2013-06-14 09:29:43 -0300

    Add preferences view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602021113) do
+ActiveRecord::Schema.define(:version => 20130607172124) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(:version => 20130602021113) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "photo"
   end
 
   create_table "user_roles", :force => true do |t|
