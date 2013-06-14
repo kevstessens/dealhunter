d086f9489fd91ec7cff09bfc02c329c573532271
laura <laura_lopez_bukovac@hotmail.com>
2013-06-14 12:10:22 -0300

    Change user menu deppending on role

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,9 +6,8 @@ class Client < ActiveRecord::Base
 
   attr_accessible :first_name, :last_name, :user_id, :address_attributes
 
-
-
   accepts_nested_attributes_for :address, :allow_destroy => true
 
   validates_presence_of :user
+
 end
