e0021e652240a7d4830301815b981756376f7b37
laura <laura_lopez_bukovac@hotmail.com>
2013-06-14 14:59:08 -0300

    Create clients_titles table

---
+++ db/migrate/20130614172947_create_clients_titles_table.rb
@@ -0,0 +1,12 @@
+class CreateClientsTitlesTable < ActiveRecord::Migration
+  def change
+    create_table :clients_titles do |t|
+      t.references :client
+      t.references :title
+
+      t.timestamps
+    end
+    add_index :clients_titles, :title_id
+    add_index :clients_titles, :client_id
+  end
+end
