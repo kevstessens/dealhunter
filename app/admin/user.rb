7b4e5ddc8dd61f10e1a04c98e7d17f7c4c009504
kevstessens <kevstessens@gmail.com>
2013-06-15 20:14:46 -0300

    fixing test

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -12,8 +12,6 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "User Info" do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
     end
     f.actions
   end
