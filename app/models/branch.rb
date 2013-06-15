1bfe8399929bda2a8a0988c8fa2cfb0b3372fec5
laura <laura_lopez_bukovac@hotmail.com>
2013-06-14 22:17:35 -0300

    Branch model

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -3,4 +3,12 @@ class Branch < ActiveRecord::Base
   has_one :address
 
   attr_accessible :address_id, :company_id, :name
+
+  validates_presence_of :name, :company
+
+  accepts_nested_attributes_for :address, :allow_destroy => true
+
+  validates :name, :presence => true, :length => { :minimum => 3, :maximum => 25}
+
+
 end
