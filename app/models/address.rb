97e234c8f13d693d843f0242b1f169242f1dd708
laura <laura_lopez_bukovac@hotmail.com>
2013-06-16 17:12:49 -0300

    Add validations and error translation to company and admin_user

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,10 +5,4 @@ class Address < ActiveRecord::Base
 
   attr_accessible :city, :country_id, :floor, :number, :state, :street, :client_id
 
-  validates :state, :presence => true, :length => { :minimum => 4, :maximum => 100}, :on => :update
-  validates :street, :presence => true, :length => { :minimum => 4, :maximum => 100}, :on => :update
-  validates :city, :presence => true, :length => { :minimum => 4, :maximum => 100}, :on => :update
-  validates :postcode, :presence => true, :length => { :minimum => 4, :maximum => 20}, :on => :update
-  validates :number, :presence => true, :numericality => true ,:length => { :minimum => 1, :maximum => 10}, :on => :update
-
 end
