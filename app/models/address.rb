26d8d51ffce60fadb5067251a93f8fefd146dcba
laura <laura_lopez_bukovac@hotmail.com>
2013-06-16 16:40:53 -0300

    Add validations to address and translate address errors

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,11 @@ class Address < ActiveRecord::Base
   belongs_to :country
 
   attr_accessible :city, :country_id, :floor, :number, :state, :street, :client_id
+
+  validates :state, :presence => true, :length => { :minimum => 4, :maximum => 100}, :on => :update
+  validates :street, :presence => true, :length => { :minimum => 4, :maximum => 100}, :on => :update
+  validates :city, :presence => true, :length => { :minimum => 4, :maximum => 100}, :on => :update
+  validates :postcode, :presence => true, :length => { :minimum => 4, :maximum => 20}, :on => :update
+  validates :number, :presence => true, :numericality => true ,:length => { :minimum => 1, :maximum => 10}, :on => :update
+
 end
