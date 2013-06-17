df1ba4a8403939090cb5fa6a2b4c3ab1ef25b4a7
laura <laura_lopez_bukovac@hotmail.com>
2013-06-17 00:07:39 -0300

    New branch

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -1,2 +1,18 @@
 class BranchesController < InheritedResources::Base
+
+  def create
+    @branch = Company.new(params[:branch])
+    @branch.company_id = current_user.company.id
+
+    respond_to do |format|
+      if @branch.save
+        format.html { redirect_to users_branches_company_user_path, notice: 'Branch was successfully created.' }
+        format.json { render json: @branch, status: :created, location: @branch }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @branch.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
--- app/models/branch.rb
+++ app/models/branch.rb
@@ -2,7 +2,7 @@ class Branch < ActiveRecord::Base
   has_one :company
   has_one :address
 
-  attr_accessible :address_id, :company_id, :name, :address_attributes
+  attr_accessible :company_id, :name,:address_attributes
 
   validates_presence_of :name, :company_id
 
@@ -11,7 +11,7 @@ class Branch < ActiveRecord::Base
   validates :name, :presence => true, :length => { :minimum => 4, :maximum => 25}
 
   def address_as_string
-    unless self.address_id.nil?
+    unless self.address.nil?
       address = Address.find_by_id(self.address_id)
       address.street + " " + address.number.to_s + " - " + address.city + ", " + address.state
     end
