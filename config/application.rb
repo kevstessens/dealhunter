6094977e26a24833338690644198b6109ba08c15
kevstessens <kevstessens@gmail.com>
2013-06-20 00:47:43 -0300

    more for heroku

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'csv'
+require 'iconv'
+require "sprockets/railtie"
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
