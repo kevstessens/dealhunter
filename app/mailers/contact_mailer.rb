84a8b6d900c69d3af7829987d844821c5b9f1c9c
kevstessens <kevstessens@gmail.com>
2013-08-10 20:03:23 -0300

    mailer y message controller

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,15 @@
+class ContactMailer < ActionMailer::Base
+
+  default :from => "admin.docnet@gmail.com"
+  default :to => "kevstessens@gmail.com"
+
+  def new_message(message)
+    @message = message
+    mail(:subject => "[Dealhunter-contacto] #{message.subject}")
+  end
+
+  def new_message_proveedor(message)
+    @message = message
+    mail(:subject => "[Dealhunter-contacto-empresa-potencial] #{message.subject}")
+  end
+end
