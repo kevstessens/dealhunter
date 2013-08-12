d6142ef048644c9a192957a71791e777b3d4994d
laura <laura_lopez_bukovac@hotmail.com>
2013-08-12 19:01:33 -0300

    Back validations and missing translations

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -32,9 +32,11 @@ ActiveAdmin.register Company do
   controller do
     def create
       super
-      @company.user.user_role_id = 1
-      @company.save!
-      NewUserMailer.new_user_email(@company.user, @company).deliver
+      unless @company.user.nil?
+        @company.user.user_role_id = 1
+        @company.save!
+        NewUserMailer.new_user_email(@company.user, @company).deliver
+      end
     end
 
   end
