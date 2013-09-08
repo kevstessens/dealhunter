e2d61f9029586dab905ba65fcaf402f3b57c23d2
laura <laura_lopez_bukovac@hotmail.com>
2013-09-08 16:59:31 -0300

    Set newsletter mailer as action mailer

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,9 +1,11 @@
-class NewsletterMailer
+class NewsletterMailer < ActionMailer::Base
   default :from => "noresponder@dealhunter.com"
+  default :to => "laura_lopez_bukovac@hotmail.com"
 
-  def newsletter_email(client)
+  def newsletter_email()
     @offers = Offer.actual.take(6)
-    mail(:to => client.user.email, :subject => "Newsletter Dealhunter")
+    mail(:subject => "Newsletter Dealhunter")
+    #mail(:to => client.user.email, :subject => "Newsletter Dealhunter")
   end
 
 end
