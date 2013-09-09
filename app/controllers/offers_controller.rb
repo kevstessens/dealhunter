6d133bacac1b0988aef48dbe01eea946b3ef3b7d
laura <laura_lopez_bukovac@hotmail.com>
2013-09-08 22:57:31 -0300

    Offers show

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -16,6 +16,7 @@ class OffersController < ApplicationController
   # GET /offers/1.json
   def show
     @offer = Offer.find(params[:id])
+    @user = current_user
 
     respond_to do |format|
       format.html # show.html.erb
