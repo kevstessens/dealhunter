3bd420e9fa0b59a60a02e3241b01cab0674e443f
laura <laura_lopez_bukovac@hotmail.com>
2013-09-17 18:34:05 -0300

    Add query for offers in newsletter

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -3,6 +3,8 @@ class NewsletterMailer < ActionMailer::Base
 
   def newsletter_email()
     @offers = Offer.actual.take(6)
+    clients_titles = ClientsTitles.select("title_id").where(:client_id => Client.where(:user_id => @user.id))
+    #@offers = Offer.actual.where(:id => OffersTitles.select("offer_id").where(:title_id => clients_titles)).all.take(6)
     mail(:to => client.user.email, :subject => "Newsletter Dealhunter")
   end
 
