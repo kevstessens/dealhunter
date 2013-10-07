a5d43ca6e8f0dce0274bbf1484a3a26e62939645
Tatiana Slodkowski <tatiana.slod@gmail.com>
2013-10-06 21:10:46 -0300

    offer's show style

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -2,10 +2,9 @@ class NewsletterMailer < ActionMailer::Base
   default :from => "noresponder@dealhunter.com"
 
   def newsletter_email(client)
-
     clients_titles = ClientsTitles.select("title_id").where(:client_id => client.id)
     @offers = Offer.actual.where(:id => OffersTitles.select("offer_id").where(:title_id => clients_titles)).all.take(6)
-    mail(:to => client.user.email, :subject => "Newsletter Dealhunter")
+    mail(:to => "galika_tati@hotmail.com", :subject => "Newsletter Dealhunter")
   end
 
 end
