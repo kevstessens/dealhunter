a3041eab314a9f917639e5ae07504fa9a1e3ac17
laura <laura_lopez_bukovac@hotmail.com>
2013-11-01 19:21:10 -0300

    Changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -118,8 +118,8 @@ class UsersController < ApplicationController
     @user = current_user
 
     @offers = Offer.where(:branch_id => Branch.where(:company_id => @user.company.id)).page(params[:page]).per(3)
-    @actual_offers = Offer.actual.page(params[:page]).per(3)
-    @old_offers = Offer.ended.page(params[:page]).per(3)
+    @actual_offers = Offer.actual.where(:branch_id => Branch.where(:company_id => @user.company.id)).page(params[:page]).per(3)
+    @old_offers = Offer.ended.where(:branch_id => Branch.where(:company_id => @user.company.id)).page(params[:page]).per(3)
   end
 
   def offers_client_user
