8b016b51335963557f19be6e6c95b03cf3230f7c
laura <laura_lopez_bukovac@hotmail.com>
2013-11-10 18:20:57 -0300

    Client registration graph for admin statistics

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -1,5 +1,37 @@
 class Statistics
-  def self.client_inscriptions
 
+  def persisted?
+    false
   end
+
+  def Statistics.client_registrations
+    client_inscriptions = Array.new
+    months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    months.each do |m|
+      client_inscriptions.push([m, 0])
+    end
+    clients = Client.all
+    clients.each do |client|
+      m = client.created_at.month
+      a = client_inscriptions[m-1]
+      a[1] += 1
+    end
+    client_inscriptions
+  end
+
+  def Statistics.company_registrations
+    companies_inscriptions = Array.new
+    months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    months.each do |m|
+      companies_inscriptions.push([m, 0])
+    end
+    companies = Company.all
+    companies.each do |company|
+      m = company.created_at.month
+      a = companies_inscriptions[m-1]
+      a[1] += 1
+    end
+    companies_inscriptions
+  end
+
 end
