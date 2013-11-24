b7f66a97a784eca4aff597f900722207bfa3e252
laura <laura_lopez_bukovac@hotmail.com>
2013-11-24 15:36:41 -0300

    secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
 
-  DealHunter::Application.config.secret_token = ENV['SECRET_TOKEN']
+
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
 
